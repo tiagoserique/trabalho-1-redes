22e33194ddc7f1515e985a21d6871af0a4b2072d
Iago Floriano <iagomfloriano@gmail.com>
2023-01-31 18:28:16 -0300

    Começo janela

--- functions-client.cpp
+++ functions-client.cpp
@@ -4,7 +4,16 @@
 // Internal functions headers
 // --------------------------
 
-unsigned int waitAck(unsigned int seq, const int &sckt);
+/*
+    @brief Waits for Ack/Nack
+
+    @param sckt (const int &) : The File Descriptor of the socket
+    @param type (const int) : The type of package to wait
+
+    @return -1 if timed out
+    @return The sequence number of the ack package otherwise
+*/
+int waitResponse( const int &sckt, const unsigned int type );
 
 
 // ---------------
@@ -13,40 +22,22 @@ unsigned int waitAck(unsigned int seq, const int &sckt);
 
 void sendMessage(const int &sckt){
     char ch {};
-    package_t start {};
     std::string message {};
     
     while ( (ch = (char) std::cin.get()) != STOP_INSERT_COMMAND ) message += ch;
 
-    initPackage(start, START_PACKAGE);
-    ssize_t ret {send(sckt, &start, sizeof(package_t), 0)}; 
-    if ( ret < 0 ){
-        std::cerr << "Error sending message" << std::endl;
-        std::cerr << std::endl;
-        std::cerr << errno << std::endl;
-        return;
-    }
     package_t * packs {divideData(
                       (void *)message.c_str(),
                       message.size(),
                       TEXT_PACKAGE,
                       0)};
 
-    unsigned int packI {0};
-    package_t currPack = packs[0];
-    while ( currPack.type != END_PACKAGE ){
-        // Send
-        currPack = packs[packI];
-        ret = send(sckt, &currPack, sizeof(package_t), 0); 
-        if ( ret < 0 ){
-            std::cerr << "Error sending message" << std::endl;
-            std::cerr << std::endl;
-            std::cerr << errno << std::endl;
-            return;
-        }
-
-        // Wait for ack ( if timed out resend )
-        packI += waitAck( (unsigned int)currPack.sequence, sckt);
+    unsigned int numSent = sendPacks(sckt, packs);
+    if ( !numSent ) {
+         std::cerr << "Error sending message" << std::endl;
+         std::cerr << std::endl;
+         std::cerr << errno << std::endl;
+         return;
     }
     std::cerr << "Successful message sent" << std::endl;
     std::cerr << std::endl;
@@ -61,6 +52,32 @@ void sendFile(const int &sckt){
     std::cerr << std::endl;
 }
 
+unsigned int sendPacks(const int &sckt, package_t * packs){
+  const int windowSize {5};
+  unsigned int currPackIndex {0};
+  // send start package to stabilish connection
+  package_t start {};
+  initPackage(start, START_PACKAGE);
+  start.sequence = (packs[0].sequence - 1) % 16;
+
+  ssize_t ret {send(sckt, &start, sizeof(package_t), 0)}; 
+  if ( ret < 0 ){
+      std::cerr << "Error sending message" << std::endl;
+      std::cerr << std::endl;
+      std::cerr << errno << std::endl;
+      return 0;
+  }
+
+  unsigned int timeoutCounter {0};
+  while ( waitResponse(sckt, ACK_PACKAGE) != start.sequence && timeoutCounter < 10){ timeoutCounter++; }
+  if (timeoutCounter >= 20) return 0;
+  std::cerr << "Estabilished connection" << std::endl;
+  // while there are still packs to send
+  while ( packs[currPackIndex].type != END_PACKAGE ){
+    currPackIndex++;
+  }
+  return packs[currPackIndex].sequence;
+}
 
 void quitProgram(bool &stop){
     stop = true;
@@ -72,36 +89,22 @@ void quitProgram(bool &stop){
 
 // Internal functions
 
-/*
-    @brief Waits for ack signal
-
-    @param seq (unsigned int) : desired sequence number of ack package
-    @param sckt (const int &) : The File Descriptor of the socket
-
-    @return 1 if received ack
-    @return 0 if timed out
-*/
-unsigned int waitAck(unsigned int seq, const int &sckt){
+int waitResponse( const int &sckt, const unsigned int type ){
     package_t ack {};
 
     do{
       ssize_t val {recv(sckt, &ack, sizeof(ack), 0)};
       if ( val == -1 && ( errno = EAGAIN)){
         std::cout << "Timed out" << std::endl;
-        return 0;
+        return -1;
       }
 
-      if ( ack.type != ACK_PACKAGE ){
+      if ( ack.type != type ){
         // TODO: Save somewhere
         continue;
       }
 
-      if ( (unsigned int)ack.sequence != seq ){
-        // TODO: Save somewhere
-        continue;
-      }
-
-      printPackage(ack);
-      return 1;
+      //printPackage(ack);
+      return ack.sequence;
     } while ( true );
 }
--- functions-client.hpp
+++ functions-client.hpp
@@ -34,6 +34,17 @@ void sendMessage(const int &sckt);
 void sendFile(const int &sckt);
 
 /*
+    @brief Sends an array of packs using sliding windows of size 5
+
+    @param sckt (const int &) : The File Descriptor of the socket
+    @param packs (package_t *) : The array of packages to be sent, the first one should not be of type START_PACKAGE, the last one being of type END_PACKAGE
+
+    @return the number of packages sent
+    @return 0 if no packages could be sent
+*/
+unsigned int sendPacks(const int &sckt, package_t * packs);
+
+/*
     @brief Quit the program
 
     @param stop (bool &) : The variable that controls the program
